05a65f4f7a22dd487f7883ece520e1da0b83b924
David Nemeth <david.nemeth@healthcareinternational.com>
2023-11-05 13:46:24 +0000

    Create UserController with comments

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        //Validate input
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        //Create user and encrypt password
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        //Create token
+        $token = $user->createToken('testapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        //Validate input
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        //Find user
+        $user = User::where('email', $request->input('email'))->first();
+
+        //Problem handling
+        if (!$user) {
+            return response(['message' => 'User not find with this email'], 422);
+        } elseif (!Hash::check($request->input('password'), $user->password)) {
+            return response(['message' => 'Incorrect password'], 422);
+        }
+
+        //Create token
+        $token = $user->createToken('testapptoken')->plainTextToken;
+
+        return response([
+            'token' => $token,
+        ], 200);
+    }
+
+    //We need logout also for security reason
+    public function logout(Request $request)
+    {
+        //Delete all token for this user
+        $request->user()->tokens()->delete();
+
+        return response([
+            'message' => 'You successfully logged out',
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 
 Route::group(['middleware'=> ['auth:sanctum']], function (){
     Route::resource('tasks', TaskController::class);
+    Route::post('/logout', [UserController::class, 'logout']);
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
